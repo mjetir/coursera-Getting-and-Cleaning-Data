73440430c00e2d436c7c1dd9e06eb571d045926c
Maria Tirronen <maria.j.e.tirronen@jyu.fi>
2015-11-20 22:32:53 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#Coursera - Getting and Cleaning Data 
+##Course Project
+
+The purpose of this project is to demonstrate collecting, working with, and cleaning a data set. The goal is to prepare tidy data that can be used for later analysis. 
+
+Here are the data for the project:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The data represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+The run_analysis.R script
+1. merges the training and the test sets to create one data set,
+2. extracts only the measurements on the mean and standard deviation for each measurement,
+3. uses descriptive activity names to name the activities in the data set,
+4. appropriately labels the data set with descriptive variable names, and
+5. from the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+CodeBook.md describes the variables, the data, and the transformations and work that is performed to clean up the data.  
---
+++ run_analysis.R
@@ -0,0 +1,63 @@
+#Read the measurement data. The data are assumed to be in the working directory.
+X_test<-read.table("UCI HAR Dataset/test/X_test.txt",header=FALSE)
+X_train<-read.table("UCI HAR Dataset/train/X_train.txt",header=FALSE)
+
+#Read the features data and set the features as column names to the measurement data.
+features<-read.table("features.txt",header=FALSE)
+colnames(X_test)<-features$V2
+colnames(X_train)<-features$V2
+
+#Extract only the measurements on the mean and standard deviation for each measurement.
+indeces<-sort(c(grep("mean\\(\\)",features$V2),grep("std\\(\\)",features$V2)))
+X_test<-X_test[,indeces]
+X_train<-X_train[,indeces]
+
+#Read the subject data and give an appropriate variable name.
+subject_test<-read.table("test/subject_test.txt",header=FALSE)
+subject_train<-read.table("train/subject_train.txt",header=FALSE)
+colnames(subject_test)<-"subject"
+colnames(subject_train)<-"subject"
+
+#Read the activity data and replace the activity codes by descriptive activity names.
+activity_labels<-read.table("activity_labels.txt",header=FALSE)
+y_test<-read.table("test/y_test.txt",header=FALSE)
+y_train<-read.table("train/y_train.txt",header=FALSE)
+colnames(y_test)<-"activity"
+colnames(y_train)<-"activity"
+y_test$activity<-activity_labels$V2[y_test$activity]
+y_train$activity<-activity_labels$V2[y_train$activity]
+
+#Create variable that tells to which group (test or train) the measurement belongs. 
+#Set the variable values for the test data.
+set<-rep("test",nrow(X_test))
+#Combine subject labels, group labels 8test or train), activities and measurements 
+#of test data into one data set.
+test<-cbind(subject_test,set,y_test,X_test)
+#Create variable that tells to which group (test or train) the measurement belongs. 
+#Set the variable values for the train data.
+set<-rep("train",nrow(X_train))
+#Combine subject labels, group labels, activities and measurements of train data 
+#into one data set.
+train<-cbind(subject_train,set,y_train,X_train)
+
+#Merge the test and train data.
+data<-rbind(test,train)
+
+#Modify the variable names of the measurements so that they are more human readable.
+variables<-colnames(data[,4:69])
+variables<-gsub("\\(\\)", "",variables)
+variables<-gsub("BodyBody", "Body",variables)
+variables<-gsub("^t","time",variables)
+variables<-gsub("^f","frequency",variables)
+variables<-gsub("Acc", "Accelerometer",variables)
+variables<-gsub("Gyro", "Gyroscope",variables)
+variables<-gsub("Mag", "Magnitude",variables)
+colnames(data)<-c(colnames(data[,1:3]),variables)
+
+#Create a data set with the average of each variable for each activity and each subject.
+data2<-aggregate(data[4:69],by=list(data$subject,data$activity),FUN=mean)
+#Set appropriate variable names.
+colnames(data2)<-c("subject","activity",paste(rep("mean-of-",length(data[4:69])),
+                                              colnames(data[4:69]),sep=""))
+
+write.table(data2,"tidyData.txt",row.name=FALSE)
